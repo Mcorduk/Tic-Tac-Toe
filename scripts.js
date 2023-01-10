fcc11f1e38f7de4f56d81ce9b6d8f5e751127452
Mcorduk <corduk.5tp@gmail.com>
2023-01-10 19:13:43 +0300

    Added Board Renderer

--- scripts.js
+++ scripts.js
@@ -1,46 +1,79 @@
-let TicTacToe = (function(){
-  let board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+const gameBoard = (() => {
+  const board = [
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"]
   ];
 
-  function markCell(row, col, value){
-    if(board[row][col] !== null){
-      //Error on already marked cells
-      console.log(`This cell is already marked, 
-      please select another cell!`)
-      return;
-    }
-    board[col][row] = value;
-    //Mark Cells
-  }
-
-  function checkColumns(player) {
+  function renderBoard() {
     for (let i = 0; i < board.length; i++) {
-        let column = board.map(row => row[i]);
-        if (column.every(cell => cell === player)) {
-            return true;
-        }
-    }
-    return false;
-  }
-
-  checkwin = (player) => {
-    for(let i = 0; i < board.length; i++){
-      if (board[i].every(cell => cell === player)) {
-        return true;
+      for (let j = 0; j < board[i].length; j++) {
+        let id = `${i}-${j}`;
+        let cell = document.getElementById(id);
+        cell.innerHTML = board[i][j];
+        cell.addEventListener("click", function() {
+          cell.innerHTML = "X";
+          board[i][j] = "X";
+        });
       }
     }
+    return;
   }
 
-  return {  
-    mark: markCell,
-    checkCol: checkColumns
-  }
+  return {
+    renderBoard
+  };
 })();
 
-const playerFactory = (name) => {
-  return {name};
-}
 
+  const createPlayer = (name, symbol) => {
+    return {
+      name,
+      symbol,
+    }
+  };
+
+
+
+  // gameLogic: game = (() => {
+
+  //   function checkWinner(){
+      
+  //     function checkColumns(player) {
+  //       for (let i = 0; i < board.length; i++) {
+  //           let column = board.map(row => row[i]);
+  //           if (column.every(cell => cell === player)) {
+  //               return true;
+  //           }
+  //       }
+  //       return false;
+  //       }
+
+  //   }})();
+
+
+// markCell: function markCell(row, col, value){
+//   if(board[row][col] !== null){
+//     console.log(`This cell is already marked, please select another cell!`);
+//     return;
+//   }
+//   board[col][row] = value;
+// }
+
+// function checkColumns(player) {
+//   for (let i = 0; i < board.length; i++) {
+//       let column = board.map(row => row[i]);
+//       if (column.every(cell => cell === player)) {
+//           return true;
+//       }
+//   }
+//   return false;
+// }
+
+// checkwin = (player) => {
+//   for(let i = 0; i < board.length; i++){
+//     if (board[i].every(cell => cell === player)) {
+//       return true;
+//     }
+//   }
+// }
