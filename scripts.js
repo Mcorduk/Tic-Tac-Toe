77d0946bb00587b3a91e21260db776df79c02f24
Mcorduk <corduk.5tp@gmail.com>
2023-01-10 20:57:59 +0300

    Add Game and Player modules

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const ticTacToe = (() => {
 
+  let currentPlayer = player1; //Test Player
+  
   const GameBoard = (() => {
     //Board Module
   let board = [
@@ -7,7 +9,6 @@ const ticTacToe = (() => {
     [null, null, null],
     [null, null, null]
   ];
-
   let cache = {};
   //cache DOM elements
   function renderBoard() {
@@ -16,6 +17,9 @@ const ticTacToe = (() => {
         let id = `${i}-${j}`;
         if (!cache[id]) {
           cache[id] = document.getElementById(id);
+          cache[id].addEventListener("click", function() {
+            Game.addMark(i, j);
+          });
         }
         cache[id].innerHTML = board[i][j];
       }
@@ -23,91 +27,46 @@ const ticTacToe = (() => {
   } 
     return {
     renderBoard,
-    cache
+    cache,
     };
-
   })();
   
-  function addMark(i, j) {
-    //Player adds respective symbol
-    if (board[i][j] === null) {
-      board[i][j] = currentPlayer.symbol;
-      cache[`${i}-${j}`].innerHTML = currentPlayer.symbol;
+  const Game = (() => {
+    function addMark(i, j) {
+      if (board[i][j] === null) {
+        board[i][j] = currentPlayer.symbol;
+        cache[`${i}-${j}`].innerHTML = currentPlayer.symbol;
+        toggleCurrentPlayer();
+        checkForWin();
+      }
     }
-  }
- 
+    function toggleCurrentPlayer() {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+    function checkForWin() {
+      // Check rows, columns, and diagonals for a win
+    }
+    return {
+      addMark,
+    };
+  })();
+
+  const Player = () => {
+    const createPlayer = (name, symbol) => {
+      //Player Factory
+      return {
+        name,
+        symbol,
+      }
+    };
+    
+    const player1 = createPlayer("player1", "X");
+    const player2 = createPlayer("player2", "O");
+  };
+
+  return {
+    GameBoard,
+  };
 })();
 
 
-const createPlayer = (name, symbol) => {
-  return {
-    name,
-    symbol,
-  }
-};
-
-const player1 = createPlayer("player1", "X");
-const player2 = createPlayer("player2", "O");
-
-let currentPlayer = player1;
-
-const createCell = (i, j, value) => {
-return {
-  i: i,
-  j: j,
-  value: value,
-  handleClick: function() {
-    if (this.value === "O") {
-      this.value = currentPlayer.symbol;
-      this.element.innerHTML = currentPlayer.symbol;
-      if(currentPlayer === player1)
-          currentPlayer = player2;
-      else
-          currentPlayer = player1;
-    }
-  }
-};
-};
-
-  // gameLogic: game = (() => {
-
-  //   function checkWinner(){
-      
-  //     function checkColumns(player) {
-  //       for (let i = 0; i < board.length; i++) {
-  //           let column = board.map(row => row[i]);
-  //           if (column.every(cell => cell === player)) {
-  //               return true;
-  //           }
-  //       }
-  //       return false;
-  //       }
-
-  //   }})();
-
-
-// markCell: function markCell(row, col, value){
-//   if(board[row][col] !== null){
-//     console.log(`This cell is already marked, please select another cell!`);
-//     return;
-//   }
-//   board[col][row] = value;
-// }
-
-// function checkColumns(player) {
-//   for (let i = 0; i < board.length; i++) {
-//       let column = board.map(row => row[i]);
-//       if (column.every(cell => cell === player)) {
-//           return true;
-//       }
-//   }
-//   return false;
-// }
-
-// checkwin = (player) => {
-//   for(let i = 0; i < board.length; i++){
-//     if (board[i].every(cell => cell === player)) {
-//       return true;
-//     }
-//   }
-// }
